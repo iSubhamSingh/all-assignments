5ed598834c72871dff4f9db721ce36d1a9249277
Subham <subhamsingh5638@gmail.com>
2024-03-06 22:59:14 +0530

    completed anagram.js assignment

--- week-1/Week-1-assignment-with-tests/01-js/easy/anagram.js
+++ week-1/Week-1-assignment-with-tests/01-js/easy/anagram.js
@@ -8,7 +8,15 @@
 */
 
 function isAnagram(str1, str2) {
+     
+    str1 = str1.toLowerCase();
+    str2 = str2.toLowerCase(); 
+    let arr1 = str1.split('');
+    let arr2 = str2.split('');
+    arr1.sort();
+    arr2.sort();
+    return arr1.join('') === arr2.join('');
 
 }
-
+ 
 module.exports = isAnagram;
